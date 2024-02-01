560a76738a25aec1c9d721f9dfd09a0be4e6abdd
aradhyatiwari <aradhya.prad@gmail.com>
2024-02-01 23:08:05 +0530

    cloudflare adapter added

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false, })]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
