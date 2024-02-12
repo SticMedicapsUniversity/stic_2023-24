e605b728635e17540adb5f896d0f600ed4b8f2c1
aradhyatiwari <aradhya.prad@gmail.com>
2024-02-12 20:50:39 +0530

    header made responsive and Main section (responsive) made

---
+++ client/.wrangler/tmp/pages-BUujv0/functionsRoutes-0.40786011956858714.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ____path___js_onRequest } from "C:\\Users\\aradh\\git\\stic_2023-24\\client\\functions\\[[path]].js"
+
+export const routes = [
+    {
+      routePath: "/:path*",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [____path___js_onRequest],
+    },
+  ]
